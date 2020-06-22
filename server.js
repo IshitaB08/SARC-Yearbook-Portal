c60fc37b6e71674ae5b4a9653edc31590019c270
IshitaB08 <f20180306@pilani.bits-pilani.ac.in>
2020-06-23 00:47:53 +0530

    prot

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const User = require('./models/user')
 const passport = require('passport')
 const keys = require('./config/keys')
 const cookieSession = require('cookie-session')
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASEURL, {
   useNewUrlParser : true,
