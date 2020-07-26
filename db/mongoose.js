f06cf87d74738c3cc217e7d176f12c82339c0de1
IshitaB08 <f20180306@pilani.bits-pilani.ac.in>
2020-07-26 12:17:18 +0530

    done6

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('', {
+mongoose.connect('mongodb://127.0.0.1:27017/sarc-portal', {
         useNewUrlParser : true,
         useCreateIndex : true,
         useUnifiedTopology : true,
